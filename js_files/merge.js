e012cfe6e7c283a185660d76bb6984180bfed001
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2023-08-20 14:23:02 +0530

    Update merge.js

--- js_files/merge.js
+++ js_files/merge.js
@@ -1,78 +1,118 @@
-async function mergeSort(arr, left, right) {
-  if (left >= right) return;
+async function merge(ele, low, mid, high){
+    console.log('In merge()');
+    console.log(`low=${low}, mid=${mid}, high=${high}`);
+    const n1 = mid - low + 1;
+    const n2 = high - mid;
+    console.log(`n1=${n1}, n2=${n2}`);
+    let left = new Array(n1);
+    let right = new Array(n2);
 
-  const mid = Math.floor((left + right) / 2);
-
-  await mergeSort(arr, left, mid);
-  await mergeSort(arr, mid + 1, right);
-
-  await merge(arr, left, mid, right);
+    for(let i = 0; i < n1; i++){
+        await waitforme(delay);
+        console.log('In merge left loop');
+        console.log(ele[low + i].style.height + ' at ' + (low+i));
+        // color
+        ele[low + i].style.background = 'orange';
+        left[i] = ele[low + i].style.height;
+    }
+    for(let i = 0; i < n2; i++){
+        await waitforme(delay);
+        console.log('In merge right loop');
+        console.log(ele[mid + 1 + i].style.height + ' at ' + (mid+1+i));
+        // color
+        ele[mid + 1 + i].style.background = 'yellow';
+        right[i] = ele[mid + 1 + i].style.height;
+    }
+    await waitforme(delay);
+    let i = 0, j = 0, k = low;
+    while(i < n1 && j < n2){
+        await waitforme(delay);
+        console.log('In merge while loop');
+        console.log(parseInt(left[i]), parseInt(right[j]));
+        
+        // To add color for which two r being compared for merging
+        
+        if(parseInt(left[i]) <= parseInt(right[j])){
+            console.log('In merge while loop if');
+            // color
+            if((n1 + n2) === ele.length){
+                ele[k].style.background = 'green';
+            }
+            else{
+                ele[k].style.background = 'lightgreen';
+            }
+            
+            ele[k].style.height = left[i];
+            i++;
+            k++;
+        }
+        else{
+            console.log('In merge while loop else');
+            // color
+            if((n1 + n2) === ele.length){
+                ele[k].style.background = 'green';
+            }
+            else{
+                ele[k].style.background = 'lightgreen';
+            } 
+            ele[k].style.height = right[j];
+            j++;
+            k++;
+        }
+    }
+    while(i < n1){
+        await waitforme(delay);
+        console.log("In while if n1 is left");
+        // color
+        if((n1 + n2) === ele.length){
+            ele[k].style.background = 'green';
+        }
+        else{
+            ele[k].style.background = 'lightgreen';
+        }
+        ele[k].style.height = left[i];
+        i++;
+        k++;
+    }
+    while(j < n2){
+        await waitforme(delay);
+        console.log("In while if n2 is left");
+        // color
+        if((n1 + n2) === ele.length){
+            ele[k].style.background = 'green';
+        }
+        else{
+            ele[k].style.background = 'lightgreen';
+        }
+        ele[k].style.height = right[j];
+        j++;
+        k++;
+    }
 }
 
-async function merge(arr, left, mid, right) {
-  const n1 = mid - left + 1;
-  const n2 = right - mid;
-
-  const leftArr = new Array(n1);
-  const rightArr = new Array(n2);
-
-  for (let i = 0; i < n1; i++) {
-      leftArr[i] = arr[left + i];
-  }
-
-  for (let i = 0; i < n2; i++) {
-      rightArr[i] = arr[mid + 1 + i];
-  }
-
-  let i = 0;
-  let j = 0;
-  let k = left;
-
-  while (i < n1 && j < n2) {
-      document.querySelectorAll(".bar")[left + i].style.background = 'blue';
-      document.querySelectorAll(".bar")[mid + 1 + j].style.background = 'blue';
-      await waitforme(delay);
-
-      if (leftArr[i] <= rightArr[j]) {
-          arr[k] = leftArr[i];
-          i++;
-      } else {
-          arr[k] = rightArr[j];
-          j++;
-      }
-      k++;
-  }
-
-  while (i < n1) {
-      arr[k] = leftArr[i];
-      i++;
-      k++;
-  }
-
-  while (j < n2) {
-      arr[k] = rightArr[j];
-      j++;
-      k++;
-  }
-
-  for (let x = left; x <= right; x++) {
-      document.querySelectorAll(".bar")[x].style.background = 'green';
-  }
-  await waitforme(delay);
+async function mergeSort(ele, l, r){
+    console.log('In mergeSort()');
+    if(l >= r){
+        console.log(`return cause just 1 elemment l=${l}, r=${r}`);
+        return;
+    }
+    const m = l + Math.floor((r - l) / 2);
+    console.log(`left=${l} mid=${m} right=${r}`, typeof(m));
+    await mergeSort(ele, l, m);
+    await mergeSort(ele, m + 1, r);
+    await merge(ele, l, m, r);
 }
 
-const mergeSortBtn = document.querySelector(".mergeSort");
-mergeSortBtn.addEventListener('click', async function () {
-  const bars = document.querySelectorAll(".bar");
-  const arr = [];
-  for (let i = 0; i < bars.length; i++) {
-      arr.push(parseInt(bars[i].style.height));
-  }
-  disableSortingBtn();
-  disableSizeSlider();
-  disableNewArrayBtn();
-  await mergeSort(arr, 0, arr.length - 1);
-  enableSortingBtn();
-  enableSizeSlider();
-  enableNewArrayBtn();
+const mergeSortbtn = document.querySelector(".mergeSort");
+mergeSortbtn.addEventListener('click', async function(){
+    let ele = document.querySelectorAll('.bar');
+    let l = 0;
+    let r = parseInt(ele.length) - 1;
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await mergeSort(ele, l, r);
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
 });
